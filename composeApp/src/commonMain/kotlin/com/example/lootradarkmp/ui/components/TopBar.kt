4ee3c2d639aa5d9ec45065b9b05c3598ee0aca33
Kartik Patel <kartikpat25@gmail.com>
2025-12-05 23:30:00 -0500

    Remove Android-only Preview annotations from TopBar

--- composeApp/src/commonMain/kotlin/com/example/lootradarkmp/ui/components/TopBar.kt
+++ composeApp/src/commonMain/kotlin/com/example/lootradarkmp/ui/components/TopBar.kt
@@ -1,14 +1,12 @@
 package com.example.lootradarkmp.ui.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
