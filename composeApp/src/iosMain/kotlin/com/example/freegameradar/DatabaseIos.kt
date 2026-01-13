d2ba775411145c0589497fe0880c4bb8c1833216
Kartik Patel <kartikpat25@gmail.com>
2026-01-12 20:17:47 -0500

    Remove iOS targets for Android-only build

--- composeApp/src/iosMain/kotlin/com/example/freegameradar/DatabaseIos.kt
+++
@@ -1,9 +0,0 @@
-package com.example.freegameradar
-
-import app.cash.sqldelight.driver.native.NativeSqliteDriver
-import com.example.freegameradar.db.GameDatabase
-
-fun createDatabase(): GameDatabase {
-    val driver = NativeSqliteDriver(GameDatabase.Schema, "loot_radar.db")
-    return GameDatabase(driver)
-}
--- composeApp/src/iosMain/kotlin/com/example/freegameradar/MainViewController.kt
+++
@@ -1,7 +0,0 @@
-package com.example.freegameradar
-
-import androidx.compose.ui.window.ComposeUIViewController
-import com.example.freegameradar.data.auth.AuthRepositoryImpl
-import com.example.freegameradar.ui.viewmodel.AuthViewModel
-
-fun MainViewController() = ComposeUIViewController { App(AuthViewModel(AuthRepositoryImpl())) }
