7afe542132d339b032fe5c27fb4c6b48e216abf2
Kartik Patel <kartikpat25@gmail.com>
2026-03-01 18:29:19 -0500

    Updated versionCode

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -79,8 +79,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.radarlabs.freegameradar"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
 
     signingConfigs {
